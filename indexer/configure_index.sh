d6d9dfb313dc383849ba2e55e13376a4180960e8
Diego Montúfar <diogonal0@gmail.com>
2015-04-26 18:09:57 +1000

    minor changes

--- indexer/configure_index.sh
+++ indexer/configure_index.sh
@@ -11,21 +11,21 @@ echo "Creating Index: $INDEX_NAME ..."
 curl -XPOST "localhost:9200/$INDEX_NAME" -d "@$INDEX_CONFIG_PATH"
 
 sleep 10
-echo "\n"
+echo
 
 #Create River-CouchDB
 echo "Creating River-CouchDB..."
 curl -XPUT "localhost:9200/_river/$INDEX_NAME/_meta" -d "@$RIVER_PATH"
-echo "\n"
+echo
 
 #####################DELETE INDEX AND RIVER###########################
 # # Delete River:
 # echo "Deleting River"
 # curl -XDELETE "http://localhost:9200/_river/$INDEX_NAME/"
 # sleep 5
-# echo "\n"
+# echo
 # # Delete Index:
 # echo "Deleting Index: $INDEX_NAME ..."
 # curl -XDELETE "http://localhost:9200/$INDEX_NAME/$INDEX_TYPE"
 # curl -XDELETE "http://localhost:9200/$INDEX_NAME"
-# echo "\n"
+# echo
